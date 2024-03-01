b9bc2c721e97219be717e3569d6c265298569379
oouuou <2743006133@qq.com>
2024-03-01 22:41:11 +0800

    Site updated: 2024-03-01 22:41:11

---
+++ js/weather.js
@@ -0,0 +1,26 @@
+<div id="he-plugin-simple"></div>
+<script>
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "D413D4",
+    "tmpSize": "16",
+    "cityColor": "2289C9",
+    "citySize": "16",
+    "aqiColor": "22A2C9",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "12",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "124cce98f12942658877cdd107ada48b"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script>
